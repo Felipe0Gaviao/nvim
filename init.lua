70fd6bba4dee27b454ca5f0786d5932b11e2b1ad
Felipe <batatasteve304@gmail.com>
2025-12-24 15:58:29 -0300

    refactor(explicitness): swap vim.o for vim.opt for better support of something like: vim.opt.completeopt that now can be a table of options instead of a string of options

--- init.lua
+++ init.lua
@@ -3,56 +3,63 @@
 -- ===============================
 
 -- UI
-vim.o.cursorline = true -- highlight current line
-vim.o.termguicolors = true -- true color support
-vim.o.signcolumn = "yes:1" -- reserve a single column
-vim.o.winborder = "rounded" -- make window borders rounded
-vim.o.wrap = false -- remove line wrap
-vim.o.scrolloff = 8 -- vertical scroll buffer
-vim.o.sidescrolloff = 8 -- horizontal scroll buffer
+vim.opt.cursorline = true -- highlight current line
+vim.opt.termguicolors = true -- true color support
+vim.opt.signcolumn = "yes:1" -- reserve a single column
+vim.opt.winborder = "rounded" -- make window borders rounded
+vim.opt.wrap = false -- remove line wrap
+vim.opt.scrolloff = 8 -- vertical scroll buffer
+vim.opt.sidescrolloff = 8 -- horizontal scroll buffer
 
 -- Numbers
-vim.o.number = true -- add numbers at the start of the line as reference
-vim.o.relativenumber = true -- add relative numbers so that i know how many number i am from each line other than the one i'm currently at
+vim.opt.number = true -- add numbers at the start of the line as reference
+vim.opt.relativenumber = true -- add relative numbers so that i know how many number i am from each line other than the one i'm currently at
 
 -- Tabs & Indentation
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true -- use spaces instead of tabs
-vim.o.smarttab = true
-vim.o.autoindent = true
-vim.o.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true -- use spaces instead of tabs
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 -- Searching
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.hlsearch = true
-vim.o.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
 
 -- Clipboard
-vim.o.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
 
 -- Split behavior
-vim.o.splitright = true
-vim.o.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
 -- Leader keys
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Misc / Performance
-vim.o.mouse = "a" -- enable mouse support
-vim.o.updatetime = 300 -- faster CursorHold and completion
-vim.o.completeopt = "menuone,noselect,popup"
-vim.o.backup = false
-vim.o.writebackup = false
-vim.o.swapfile = false -- no swapfiles
-vim.o.undofile = true -- persistent undo
-vim.o.undodir = vim.fn.stdpath("state") .. "/undo"
-vim.o.ttyfast = true -- terminal optimizations
+vim.opt.mouse = "a" -- enable mouse support
+vim.opt.updatetime = 300 -- faster CursorHold and completion
+vim.opt.completeopt = { "menuone", "noselect", "popup" }
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false -- no swapfiles
+vim.opt.undofile = true -- persistent undo
+vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
+vim.opt.ttyfast = true -- terminal optimizations
 
 -- Diagnostic
 vim.diagnostic.config({ virtual_text = true }) -- inline diagnostics
+vim.api.nvim_create_autocmd("CursorHold", {
+	group = vim.api.nvim_create_augroup("DiagGroup", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.diagnostic.open_float(nil, { focus = false })
+	end,
+}) -- auto open floating diagnostics
 
 -- ===============================
 -- Load Colorscheme Separately
