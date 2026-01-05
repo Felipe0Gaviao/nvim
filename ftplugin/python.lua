4c2e451cba5148a109a2602b55c37a264f729c82
Felipe <batatasteve304@gmail.com>
2026-01-05 17:58:23 -0300

    chore(python): use the global 'py' command instead of 'python', 'py' defaults to whatever version of python is loaded in the moment or installed in the global enviroment

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("n", "<leader>r", function()
 	local file = vim.fn.expand("%")
-	require("toggleterm").exec("\x15python " .. file, 1)
+	require("toggleterm").exec("\x15py " .. file, 1)
 end, { desc = "Run current python file" })
