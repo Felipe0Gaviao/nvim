f16aa8ad1da200a9cc09ca502d2d944e976d0d89
Felipe <batatasteve304@gmail.com>
2026-01-24 19:30:48 -0300

    chore(conform): add the rest of the ruff toolchain to the python formatting config and change format_on_save.timeout_ms to recommended setting

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -7,7 +7,7 @@ Pack({
 require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
-		python = { "ruff_format" },
+		python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
 		javascript = { "prettier" },
 		typescript = { "prettier" },
 		toml = { "taplo" },
@@ -17,7 +17,7 @@ require("conform").setup({
 	},
 
 	format_on_save = {
-		timeout_ms = 1000,
 		lsp_fallback = true,
+		timeout_ms = 500,
 	},
 })
