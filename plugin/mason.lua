3c0b2f963be4d0a7fe838375859d0a6341bbdc8a
Felipe <batatasteve304@gmail.com>
2026-02-22 20:01:40 -0300

    feat(linting): removed linting plugins: `nvim-lint` and `mason-nvim-lint`

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,10 +1,8 @@
 Pack({
 	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/mason-org/mason.nvim",
-	"https://github.com/mfussenegger/nvim-lint",
 	"https://github.com/mason-org/mason-lspconfig.nvim",
 	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
-	"https://github.com/rshkarin/mason-nvim-lint",
 })
 
 require("mason").setup()
@@ -27,11 +25,8 @@ require("mason-tool-installer").setup({
 		-- Python
 		"basedpyright",
 
-		-- (Type|Java)script
+		-- Web Things
 		"vtsls",
-
-		-- JSON
-		"json-lsp",
 
 		-- Markdown
 		"markdown-oxide",
@@ -55,28 +50,4 @@ require("mason-tool-installer").setup({
 	},
 	auto_update = true,
 	run_on_start = true,
-})
-
-local lint = require("lint")
-lint.linters_by_ft = {
-	python = { "ruff" },
-}
-
-vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
-	callback = function()
-		-- try_lint without arguments runs the linters defined in `linters_by_ft`
-		-- for the current filetype
-		lint.try_lint()
-		lint.try_lint("cspell")
-	end,
-})
-
-require("mason-nvim-lint").setup({
-	ensure_installed = {
-		-- general
-		"cspell",
-		-- python
-		"ruff",
-	},
-	automatic_installation = true,
 })
