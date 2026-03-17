aa59ee1533c3c534bcc5c24da0b4e9424bf4db68
Felipe <batatasteve304@gmail.com>
2026-03-17 18:56:22 -0300

    feat(snacks): enabled snacks.nvim lazygit integration

--- plugin/snacks.lua
+++ plugin/snacks.lua
@@ -3,3 +3,11 @@ Pack({
 })
 
 local Snacks = require("snacks")
+Snacks.setup({
+	lazygit = {
+		enabled = true,
+	},
+})
+
+Map("n", "<leader>gg", Snacks.lazygit.open, { desc = "Lazygit Snacks Integration" })
+
