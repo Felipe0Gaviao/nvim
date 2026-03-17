488f482b11a04e6f3612aadd363901561a548db2
Felipe <batatasteve304@gmail.com>
2026-03-17 19:23:44 -0300

    refactor(snacks|toggleterm): remove toggle term plugin and enable terminal snacks.nvim plugin

--- plugin/snacks.lua
+++ plugin/snacks.lua
@@ -16,9 +16,42 @@ Snacks.setup({
 	rename = {
 		enabled = true,
 	},
+	terminal = {
+		enabled = true,
+		win = {
+			style = "float",
+			border = "rounded",
+			width = 0.8,
+			height = 0.8,
+			keys = {
+				term_normal = {
+					"<esc>",
+					function(self)
+						self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+						if self.esc_timer:is_active() then
+							self.esc_timer:stop()
+							vim.cmd("stopinsert")
+						else
+							self.esc_timer:start(200, 0, function() end)
+							return "<esc>"
+						end
+					end,
+					mode = "t",
+					expr = true,
+					desc = "Double escape to normal mode",
+				},
+			},
+		},
+	},
 })
 
 Map("n", "<leader>gg", Snacks.lazygit.open, { desc = "Lazygit Snacks Integration" })
+Map({ "n", "t" }, [[<C-\>]], function()
+	Snacks.terminal.toggle()
+end)
+Map("n", "<leader>t", function()
+	Snacks.terminal.toggle()
+end)
 
 vim.api.nvim_create_autocmd("User", {
 	pattern = "OilActionsPost",
