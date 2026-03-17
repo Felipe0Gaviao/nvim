73e454a173242f236d7872c28a21f48c3213dd4e
Felipe <batatasteve304@gmail.com>
2026-03-17 18:56:39 -0300

    added some more stuff

--- plugin/snacks.lua
+++ plugin/snacks.lua
@@ -4,10 +4,27 @@ Pack({
 
 local Snacks = require("snacks")
 Snacks.setup({
+	bigfile = {
+		enabled = true,
+	},
+	git = {
+		enabled = true,
+	},
 	lazygit = {
+		enabled = true,
+	},
+	rename = {
 		enabled = true,
 	},
 })
 
 Map("n", "<leader>gg", Snacks.lazygit.open, { desc = "Lazygit Snacks Integration" })
 
+vim.api.nvim_create_autocmd("User", {
+	pattern = "OilActionsPost",
+	callback = function(event)
+		if event.data.actions[1].type == "move" then
+			Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+		end
+	end,
+})
