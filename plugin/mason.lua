5e736f3f42025fe6b4b8b6157146920c71b4d922
Felipe <batatasteve304@gmail.com>
2026-04-11 23:58:25 -0300

    refactor(mason): handle mason lazy loading with lze and separate ensure_installed for readability

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -4,54 +4,38 @@ Pack({
 	"https://github.com/mason-org/mason-lspconfig.nvim",
 	"https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
 })
+local lsps = {
+	"lua_ls",
+	"basedpyright",
+	"vtsls",
+	"markdown-oxide",
+	"rust-analyzer",
+}
 
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("mason-tool-installer").setup({
-	ensure_installed = {
-		-----------
-		-- LSPs
-		-----------
-
-		-- Lua
-		"lua_ls",
-
-		-- Python
-		"basedpyright",
-
-		-- Web Things
-		"vtsls",
-
-		-- Markdown
-		"markdown-oxide",
-
-		-- Rust
-		"rust-analyzer",
-
-		---------------------------
-		-- Formatters
-		---------------------------
-
-		-- Web-related code styling
-		"biome",
-
-		-- Neovim lua config editing
-		"stylua",
-
-		-- TOML editing
-		"taplo",
-
-		-- Python
-		"ruff",
-
-		-- Jinja formatter
-		-- Might be necessary later
-		"djlint",
-	},
-	auto_update = true,
-	run_on_start = true,
-})
-
+local formatters = {
+	"biome",
+	"stylua",
+	"taplo",
+	"ruff",
+	"djlint",
+}
+-- Make sure taplo loads the squemas if there is one
 vim.lsp.config("taplo", {
 	root_dir = vim.fn.expand("~"),
+})
+
+require("lze").load({
+	{
+		"mason.nvim",
+		event = "DeferredUIEnter",
+		after = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup()
+			require("mason-tool-installer").setup({
+				ensure_installed = vim.list_extend(lsps, formatters),
+				auto_update = true,
+				run_on_start = true,
+			})
+		end,
+	},
 })
