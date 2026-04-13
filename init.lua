e55e318bdbdd1c51892f31423cfd72dff576a30e
Felipe <batatasteve304@gmail.com>
2026-04-12 21:25:49 -0300

    some changes

--- init.lua
+++ init.lua
@@ -47,14 +47,8 @@ Pack({
 	"https://github.com/rachartier/tiny-cmdline.nvim",
 	-- Installing mini.nvim to deal with some of those basics for me with mini.basics
 	"https://github.com/nvim-mini/mini.nvim",
-})
-
-vim.g.lze = {
-	verbose = true,
-}
-
-require("tiny-cmdline").setup({
-	on_reposition = require("tiny-cmdline").adapters.blink,
+	-- Installing Snacks here to set some things available in this file
+	"https://github.com/folke/snacks.nvim",
 })
 
 require("mini.basics").setup({
@@ -74,22 +68,14 @@ require("mini.basics").setup({
 	},
 	silent = true,
 })
-require("mini.icons").setup()
-require("mini.indentscope").setup({
-	draw = {
-		animation = require("mini.indentscope").gen_animation.none(), -- disable scope animation
-	},
-}) -- highlight indentation scope, really good for things like dealing with lengthy HTML and function bodies
-require("mini.move").setup() -- move things (use Alt + the direction)
-require("mini.notify").setup({
-	lsp_progress = {
-		enable = false, -- disable lsp notifications of things like "loading workspace"
-	},
-}) -- adds better notifications
-require("mini.operators").setup() -- not sure if i'm going to use this enough to be worth it, remember keymap "g="
-require("mini.pairs").setup() -- auto pairs
-require("mini.statusline").setup() -- changes the statusline at the bottom of the window
-require("mini.surround").setup() -- surround text by selecting in visual mode and pressing "sa"
+
+vim.g.lze = {
+	verbose = true,
+}
+
+require("tiny-cmdline").setup({
+	on_reposition = require("tiny-cmdline").adapters.blink,
+})
 
 -- ===============================
 -- Core Config Options
