c6c543ba6893907cef0fa209cf52d878f70efed5
Felipe <batatasteve304@gmail.com>
2026-05-22 14:52:12 -0300

    chane how vim automated lockfile commiting works

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -22,36 +22,48 @@ api.nvim_create_autocmd("BufWritePost", {
 })
 
 -- Automate nvim-pack-lock.json chore commit
+local timer = vim.uv.new_timer()
 vim.api.nvim_create_autocmd("PackChanged", {
-	---@param ev {data: vim.event.packchanged.data}
-	callback = function(ev)
-		local name = ev.data.spec.name
-		local kind = ev.data.kind
-		local msg = string.format("chore(plugins): %s %s", kind, name)
-		local config = fn.stdpath("config")
+	callback = function()
+		timer = timer or vim.uv.new_timer()
+		timer:stop()
+		timer:start(
+			10000,
+			0,
+			vim.schedule_wrap(function()
+				local config = vim.fn.stdpath("config")
+				local result = vim.system({
+					"git",
+					"-C",
+					config,
+					"add",
+					"nvim-pack-lock.json",
+				}, { text = true }):wait()
 
-		vim.defer_fn(function()
-			local result = vim.system({
-				"git",
-				"-C",
-				config,
-				"commit",
-				"--only",
-				"nvim-pack-lock.json",
-				"-m",
-				msg,
-			}, { text = true }):wait()
-
-			if result.code ~= 0 then
-				local output = (result.stdout or "") .. (result.stderr or "")
-				if not output:find("nothing to commit") then
-					vim.notify("lockfile commit failed:\n" .. result.stderr, vim.log.levels.ERROR)
+				if result.code ~= 0 then
+					vim.notify("lockfile stage failed:\n" .. result.stderr, vim.log.levels.ERROR)
+					return
 				end
-			end
-		end, 500)
+
+				result = vim.system({
+					"git",
+					"-C",
+					config,
+					"commit",
+					"-m",
+					"chore(plugins): update lockfile",
+				}, { text = true }):wait()
+
+				if result.code ~= 0 then
+					local output = (result.stdout or "") .. (result.stderr or "")
+					if not output:find("nothing to commit") then
+						vim.notify("lockfile commit failed:\n" .. result.stderr, vim.log.levels.ERROR)
+					end
+				end
+			end)
+		)
 	end,
 })
-
 vim.api.nvim_create_user_command("PackUpdate", function()
 	vim.pack.update()
 end, { desc = "Update all plugins" })
