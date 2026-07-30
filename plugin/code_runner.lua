700f647aaed422bb84ae81f9545293b82470f458
Felipe <batatasteve304@gmail.com>
2026-07-30 20:20:27 -0300

    update update code_runner

--- plugin/code_runner.lua
+++ plugin/code_runner.lua
@@ -11,8 +11,25 @@ require("lze").load({
 		"code_runner.nvim",
 		ft = vim.tbl_keys(runners),
 		after = function()
-			require("code_runner").setup({ filetype = runners })
-			vim.keymap.set("n", "<leader>rr", "<cmd>RunCode<cr>", { desc = "Run current file" })
+			require("code_runner").setup({
+				mode = "snacks",
+				filetype = runners,
+				root_markers = {
+					{ "PINCE.sh", "sh PINCE.sh" },
+				},
+			})
+			vim.keymap.set(
+				"n",
+				"<leader>rr",
+				"<cmd>RunCode<cr>",
+				{ noremap = true, silent = false, desc = "Run current file" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>rp",
+				"<cmd>RunProject<cr>",
+				{ noremap = true, silent = false, desc = "Run current project" }
+			)
 		end,
 	},
 })
